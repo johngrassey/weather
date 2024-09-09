cf6160335e599353a0ebdafbd75c150662a08b25
John Grassey <johngrassey@gmail.com>
2024-09-08 21:53:38 -0400

    Functions to Pull Only Necessary Data

--- javascript.js
+++ javascript.js
@@ -4,6 +4,33 @@ async function getWeather(address) {
   );
   const weather = await response.json();
   console.log(weather);
+  todayWeather(weather);
+  fiveDayForecast(weather);
+}
+
+function todayWeather(weather) {
+  const currentConditions = {
+    conditions: weather.currentConditions.conditions,
+    temp: weather.currentConditions.temp,
+    feelsLike: weather.currentConditions.feelslike,
+  };
+  console.log(currentConditions);
+}
+
+function fiveDayForecast(weather) {
+  const forecast = [];
+  for (let i = 0; i <= 5; i++) {
+    const futureConditions = {
+      date: weather.days[i].datetime,
+      conditions: weather.days[i].conditions,
+      lowTemp: weather.days[i].tempmin,
+      highTemp: weather.days[i].tempmax,
+      feelsLike: weather.days[i].feelslike,
+      description: weather.days[i].description,
+    };
+    forecast.push(futureConditions);
+  }
+  console.log(forecast);
 }
 
 getWeather("01520");
