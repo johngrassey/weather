e853f2d65b7d8bb7f71e2ea062adeaf5c63e9985
John Grassey <johngrassey@gmail.com>
2024-09-10 12:51:45 -0400

    Add Webpack Config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import { parse, format } from "date-fns";
+import "./styles.css";
+//import "./icons/clear-day.svg";
+
 async function getWeather(address) {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${address}?key=A55MNK7S97VENY2ZUEWCWZSZR`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${address}?key=A55MNK7S97VENY2ZUEWCWZSZR`,
+    { mode: "cors" }
   );
   const weather = await response.json();
   console.log(weather);
@@ -24,12 +29,13 @@ function fiveDayForecast(weather) {
   const forecast = [];
   for (let i = 0; i <= 5; i++) {
     const futureConditions = {
-      date: weather.days[i].datetime,
+      //date: weather.days[i].datetime,
+      date: parse(weather.days[i].datetime, "yyyy-MM-dd", new Date()),
       conditions: weather.days[i].conditions,
       lowTemp: weather.days[i].tempmin,
       highTemp: weather.days[i].tempmax,
       description: weather.days[i].conditions,
-      icon: `icons/${weather.days[i].icon}.svg`,
+      icon: `/icons/${weather.days[i].icon}.svg`,
     };
     forecast.push(futureConditions);
   }
@@ -59,19 +65,19 @@ async function renderPage(location) {
   const forecast = fiveDayForecast(weather);
 
   const forecastDiv = document.querySelector(".forecast");
-  for (days in forecast) {
+  for (const days of forecast) {
     const day = document.createElement("div");
     day.classList.add("day");
-    day.textContent = forecast[days].date;
+    day.textContent = format(days.date, "eeee");
 
     const icon = document.createElement("img");
     icon.classList.add("forecastimg");
-    icon.setAttribute("src", forecast[days].icon);
+    icon.setAttribute("src", days.icon);
     day.appendChild(icon);
 
     const description = document.createElement("div");
     description.classList.add("description");
-    description.textContent = forecast[days].description;
+    description.textContent = days.description;
     day.appendChild(description);
 
     const temps = document.createElement("div");
@@ -79,12 +85,12 @@ async function renderPage(location) {
 
     const highTemp = document.createElement("div");
     highTemp.classList.add("hightemp");
-    highTemp.textContent = forecast[days].highTemp;
+    highTemp.textContent = days.highTemp;
     temps.appendChild(highTemp);
 
     const lowTemp = document.createElement("div");
     lowTemp.classList.add("hightemp");
-    lowTemp.textContent = forecast[days].lowTemp;
+    lowTemp.textContent = days.lowTemp;
     temps.appendChild(lowTemp);
 
     day.appendChild(temps);
